a430c49c8f5ca59e51bde0dcbbc001522cec2d89
linpc526@outlook.com <lin526@outlook.com>
2019-09-16 15:49:07 +0800

    9.16

--- node-student/nodeLesson7.js
+++ node-student/nodeLesson7.js
@@ -3,9 +3,11 @@ var fs = require('fs')
 var url = require('url');
 var server = http.createServer((request, response) => {
     var config = require('./server.config');
-    // console.log(request.url);
-    // console.log(url.parse(request.url).pathname, '路径');
-    // console.log(url.parse(request.url, true).query, '参数');
+    var globalConfig = config.toString();
+    console.log(globalConfig)
+        // console.log(request.url);
+        // console.log(url.parse(request.url).pathname, '路径');
+        // console.log(url.parse(request.url, true).query, '参数');
     response.writeHead(200);
     response.write("<html><body>hello wrold</body></html>");
     response.end();
